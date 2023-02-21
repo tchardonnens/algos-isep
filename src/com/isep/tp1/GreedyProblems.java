7828fc2c6698d36672e6016228a79d6af1943e74
Thomas Chardo <tchardonnens@juniorisep.com>
2023-02-21 15:56:57 +0100

    Add String permute pb

---
+++ src/com/isep/tp1/GreedyProblems.java
@@ -0,0 +1,48 @@
+package com.isep.tp1;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class GreedyProblems {
+
+    int MaxSeqProduct(int n,...,...){
+        int prod=0;
+        for(int i=0;i<n;i++){
+
+        }
+
+        return prod;
+
+    }
+	// you may need sort function
+    void quickSort(List<Integer> , int n){
+        #for(....)
+            #for(....)
+                #if(...){
+                    #.....
+                #}
+    }
+
+
+    public static void main(String args[]){
+        int n;
+        #......;
+        GreedyProblems obj= new GreedyProblems();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter value of n");
+        n=sc.nextInt();
+        #.....;
+        #.....;
+		#.....;
+		#.....;
+
+        System.out.println("Enter the clicks");
+        #.....
+
+        System.out.println("Enter the prices");
+        #.....
+		#.....
+        System.out.println("MAX profit available = "+obj.MaxSeqProduct(n,...,...));
+
+    }
+}
