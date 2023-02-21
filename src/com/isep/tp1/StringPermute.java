1ec5779fe2b0f4d60b29033315e2d85996bf1482
Thomas Chardo <tchardonnens@juniorisep.com>
2023-02-21 16:08:16 +0100

    permuteN

--- src/com/isep/tp1/StringPermute.java
+++ src/com/isep/tp1/StringPermute.java
@@ -1,6 +1,5 @@
 package com.isep.tp1;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class StringPermute {
     public static ArrayList<String> Permutations(String uniqueCharStr) {
@@ -11,19 +10,31 @@ public class StringPermute {
                 if (i==j){
                     continue;
                 }
-                StringBuilder sb = new StringBuilder();
-                sb.append(uniqueCharStr.charAt(i));
-                sb.append(uniqueCharStr.charAt(j));
-                permutations.add(sb.toString());
+                permutations.add(uniqueCharStr.charAt(i)+""+uniqueCharStr.charAt(j));
             }
         }
         return permutations;
     }
+
+    public static ArrayList<String> PermutationsN(String str, String build, ArrayList<String> results) {
+
+        if (str.length() == 0) {
+            results.add(build);
+            return results;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            String rest = str.substring(0, i) + str.substring(i + 1);
+            PermutationsN(rest, build + c, results);
+        }
+        return results;
+    }
+
     public static void main(String args[]){
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter a string: ");
-        String str =sc.nextLine();
         StringPermute permutes = new StringPermute();
-        System.out.println("Permutations: " + permutes.Permutations(str));
+        System.out.println("Permutations: " + Permutations("ab"));
+        ArrayList<String> results = new ArrayList<>();
+        System.out.println("Permutations: " + PermutationsN("abcdef", "", results));
     }
 }
